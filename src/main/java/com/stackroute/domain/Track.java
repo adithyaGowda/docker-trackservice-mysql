73a366815eb579f92c7b036b4c9cee8cfe00e0d4
adithyaGowda <adithyagowda5696@gmail.com>
2019-08-07 07:43:47 +0530

    adding spring boot app source

---
+++ src/main/java/com/stackroute/domain/Track.java
@@ -0,0 +1,22 @@
+package com.stackroute.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Track {
+
+    @Id
+    private int id;
+    private String name;
+    private String comments;
+
+
+}
